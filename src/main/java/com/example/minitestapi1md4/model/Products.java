6959420eba00f847781f0d0d668342a2ddec4406
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-06-29 08:43:53 +0700

    .

---
+++ src/main/java/com/example/minitestapi1md4/model/Products.java
@@ -0,0 +1,10 @@
+package com.example.minitestapi1md4.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+@Entity
+public class Products {
+    @Id
+    @GeneratedValue(generator = )
+}
--- src/main/java/com/example/minitestapi1md4/service/IOrderDetailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.minitestapi1md4.service;
-
-import com.example.minitestapi1md4.model.OrderDetail;
-
-public interface IOrderDetailService extends IService<OrderDetail>{
-}
